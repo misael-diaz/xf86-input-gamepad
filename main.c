2f44b52e112829309d3b581647b032d93c0fcdaa
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2026-07-01 13:58:00 -0400

    handles events in blocking mode
    NOTES
    quites when the user hits the start button
    the start button code has been hardcoded, not meant for general purpose
    use

---
+++ main.c
@@ -0,0 +1,87 @@
+#include <linux/joystick.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+
+int main()
+{
+	errno = 0;
+	struct js_event ev = {};
+	int fd = open("/dev/input/js0", O_RDONLY);
+	if (-1 == fd) {
+		fprintf(stderr, "%s\n", "error: failed to open gamepad");
+		if (errno) {
+			fprintf(stderr, "%s\n", strerror(errno));
+		}
+		_exit(1);
+	}
+
+	errno = 0;
+	int version = -1;
+	int64_t rc = ioctl(fd, JSIOCGVERSION, &version);
+	if (-1 == rc) {
+		fprintf(stderr, "%s\n", "error: failed to obtain joystick driver version");
+		if (errno) {
+			fprintf(stderr, "%s\n", strerror(errno));
+		}
+		_exit(1);
+	}
+	int const major_version = ((version >> 16) & 0xff);
+	int const minor_version = ((version >>  8) & 0xff);
+	int const patch_version = ((version >>  0) & 0xff);
+	fprintf(stdout, "driver-version: %d.%d.%d\n", major_version, minor_version, patch_version);
+	if (major_version < 1) {
+		fprintf(stderr, "%s\n", "error: unsupported version");
+		_exit(1);
+	}
+
+	errno = 0;
+	char name_gamepad[256];
+	memset(name_gamepad, 0, sizeof(name_gamepad));
+	rc = ioctl(fd, JSIOCGNAME(sizeof(name_gamepad)), name_gamepad);
+	if (-1 == rc) {
+		fprintf(stderr, "%s\n", "error: failed to query gamepad name");
+		if (errno) {
+			fprintf(stderr, "%s\n", strerror(errno));
+		}
+		_exit(1);
+	}
+	fprintf(stdout, "gamepad: %s\n", name_gamepad);
+
+	errno = 0;
+
+	while (1) {
+		rc = read(fd, &ev, sizeof(ev));
+		if (-1 == rc) {
+			fprintf(stderr, "%s\n", "error: failed to read joystick event");
+			if (errno) {
+				fprintf(stderr, "%s\n", strerror(errno));
+			}
+			_exit(1);
+		}
+		else if (sizeof(ev) != rc) {
+			fprintf(stderr, "%s\n", "warning: partial reading of joystick event");
+		}
+		else {
+			int64_t bytes_read = rc;
+			int64_t time = ev.time;
+			int64_t value = ev.value;
+			int64_t type = ev.type;
+			int64_t number = ev.number;
+			fprintf(stdout, "bytes: %ld time: %ld value: %ld type: %ld number: %ld\n", bytes_read, time, value, type, number);
+			if ((0 == value) && (1 == type) && (9 == number)) {
+				fprintf(stdout, "%s\n", "quitting upon user request");
+				break;
+			}
+		}
+	}
+
+	close(fd);
+	return 0;
+}
