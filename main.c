a58f4399148224f093d0d330148963761174054b
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2026-07-28 17:12:52 -0400

    drv: fixme implicit statx() declaration
    NOTES
    I find surprising that GCC complains about it despite us adding the
    required header linux/stat.h

--- main.c
+++ main.c
@@ -465,6 +465,7 @@ static int GamepadCorePreInit(
 	errno = 0;
 	struct statx stx = {};
 	int const ignore_dirfd = -1; // the kernel will ignore it because we are providing a full path
+	// FIXME: GCC claims that statx() lacks an explicit function declaration and I find that surprising because linux/stat.h has been included to check again
 	rc = statx(ignore_dirfd, updated_devname, 0, STATX_BASIC_STATS, &stx);
 	if (-1 == rc) {
 		xf86Msg(X_ERROR, "[%s] failed to query device status: %s\n", GAMEPAD_DRIVER_NAME, updated_devname);
