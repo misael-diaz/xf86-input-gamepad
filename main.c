934a7b4c18e1043e651b995c7f01c120ba5b02fc
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2026-08-03 15:52:25 -0400

    drv: todo done: not needed for gamepad

--- main.c
+++ main.c
@@ -945,7 +945,7 @@ static int GamepadCorePreInit(
 
 	info_gamepad->device_control = GamepadDevCtrlProc;
 	info_gamepad->read_input = GamepadDevRead;
-	info_gamepad->control_proc = NULL; // NOTE: `control_proc` only needed if we intend to support DEVICE_CORE, DEVICE_ABS_CALIB, DEVICE_ABS_AREA (this is what I know from reading the xserver source code but what modes are these are still elusive TODO: research this)
+	info_gamepad->control_proc = NULL; // NOTE: `control_proc` only needed if we intend to support DEVICE_CORE, DEVICE_ABS_CALIB, DEVICE_ABS_AREA
 	info_gamepad->switch_mode = NULL; // NOTE: only needed for devices that can switch reporting relative to absolute position
 	if (info_gamepad->drv) {
 		xf86Msg(X_DEBUG, "[%s] driver: %s\n", GAMEPAD_DRIVER_NAME, info_gamepad->drv->driverName);
